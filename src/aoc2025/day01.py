43d4d881255c007e090943c4dc7822905d2b7bee
Jacob Lorenzen <jacob@lorenzen.me>
2025-12-06 09:20:15 +0100

    chore: solution for day01 part 1

--- src/aoc2025/day01.py
+++ src/aoc2025/day01.py
@@ -1,10 +1,65 @@
 """Day 1: Advent of Code 2025"""
 
-from typing import List
+from typing import List, NamedTuple
+from enum import Enum
 
+class Direction(Enum):
+    LEFT = 1
+    RIGHT = 2
+
+class Rotation(NamedTuple):
+    direction: Direction
+    times: int
+
+class Dial():
+    def __init__(self, start_position: int = 50, max_position: int = 99):
+        self.position = start_position
+        self.MAX_POSITION = max_position
+        self.zero_based_position = 0
+
+    def rotate(self, rotation: Rotation):
+        # make sure rotation times is within bounds
+        times = rotation.times % (self.MAX_POSITION + 1)
+        if rotation.direction == Direction.LEFT:
+            self.position -= times
+        else:
+            self.position += times
+
+        # handle out-of-bounds after the rotation
+        if self.position < 0:
+            self.position += self.MAX_POSITION + 1
+        elif self.position > self.MAX_POSITION:
+            self.position -= self.MAX_POSITION + 1
+
+        # we should always be within bounds here
+        assert 0 <= self.position <= self.MAX_POSITION
+
+        if self.position == 0:
+            self.zero_based_position += 1
+
+    def zero_based_positions(self) -> int:
+        return self.zero_based_position
+
+def _parse_input(data: List[str]) -> List[Rotation]:
+    rotations = []
+    for line in data:
+        direction, times = line[0], int(line[1:])
+        if direction == 'L':
+            rotations.append(Rotation(Direction.LEFT, times))
+        elif direction == 'R':
+            rotations.append(Rotation(Direction.RIGHT, times))
+        else:
+            raise ValueError(f"Invalid direction: {direction}")
+
+    return rotations
 
 def part1(data: List[str]) -> int:
-    return 0
+    dial = Dial(50, 99)
+    rotations = _parse_input(data)
+    for rotation in rotations:
+        dial.rotate(rotation)
+
+    return dial.zero_based_positions()
 
 
 def part2(data):
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -3,12 +3,17 @@
 from aoc2025.day01 import part1, part2
 
 
-def test_part1(read_input):
+def test_part1a(read_input):
     data = read_input("data/day01_part1.txt")
     expected = 3
     actual = part1(data)
     assert actual == expected
 
+def test_part1(read_input):
+    data = read_input("data/day01.txt")
+    expected = 1048
+    actual = part1(data)
+    assert actual == expected
 
 def test_part2(read_input):
     pass
