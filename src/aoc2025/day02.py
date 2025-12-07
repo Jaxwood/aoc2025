32916cd40b3f9101cde96db86f328593dc807ccf
Jacob Lorenzen <jacob@lorenzen.me>
2025-12-07 11:38:44 +0100

    chore: solution for day02 part 1

--- src/aoc2025/day02.py
+++ src/aoc2025/day02.py
@@ -1,7 +1,28 @@
 """Day 2: Advent of Code 2025"""
 
-from typing import List
+
+def is_invalid(n: int) -> bool:
+    """Number is invalid if number is repeated exactly twice, e.g., 11, 22, 6464, 123123 etc."""
+    str_n = str(n)
+    length = len(str_n)
+
+    if length % 2 != 0:
+        return False
+
+    half_length = length // 2
+    first_half = str_n[:half_length]
+    second_half = str_n[half_length:]
+
+    return first_half == second_half
 
 
-def part1(data: List[str]) -> int:
-    return len(data)
+def part1(data: str) -> int:
+    ranges = data.split(",")
+    invalid_numbers = []
+    for r in ranges:
+        bounds = r.split("-")
+        start, end = int(bounds[0]), int(bounds[1])
+        for n in range(start, end + 1):
+            if is_invalid(n):
+                invalid_numbers.append(n)
+    return sum(invalid_numbers)
--- tests/test_day02.py
+++ tests/test_day02.py
@@ -1,10 +1,37 @@
 """Tests for Day 2"""
 
-from aoc2025.day02 import part1
+import pytest
+from aoc2025.day02 import part1, is_invalid
 
 
 def test_part1a(read_input):
     data = read_input("data/day02_part1.txt")
     expected = 1227775554
-    actual = part1(data)
+    actual = part1(data[0])
     assert actual == expected
+
+
+def test_part1(read_input):
+    data = read_input("data/day02.txt")
+    expected = 21898734247
+    actual = part1(data[0])
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "start,end,expected",
+    [
+        (11, 22, [11, 22]),
+        (95, 115, [99]),
+        (998, 1012, [1010]),
+        (1188511880, 1188511890, [1188511885]),
+        (222220, 222224, [222222]),
+        (1698522, 1698528, []),
+        (446443, 446449, [446446]),
+        (38593856, 38593862, [38593859]),
+    ],
+)
+def test_is_invalid(start, end, expected):
+    for number in range(start, end + 1):
+        expected_result = number in expected
+        assert is_invalid(number) == expected_result
