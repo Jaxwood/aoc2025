12166f456ec0106d22822c3bd23241381e11a313
Jacob Lorenzen <jacob.lorenzen@velux.com>
2026-07-26 10:48:12 +0200

    chore: solution for day08 part 1

--- src/aoc2025/day08.py
+++ src/aoc2025/day08.py
@@ -1,10 +1,76 @@
 """Day 8: Advent of Code 2025"""
 
-from typing import List
+import heapq
+from typing import List, Tuple
+
+
+class UnionFind:
+    def __init__(self, size: int):
+        self.parent = list(range(size))
+        self.rank = [0] * size
+
+    def find(self, x: int) -> int:
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, a: int, b: int) -> None:
+        root_a = self.find(a)
+        root_b = self.find(b)
+        if root_a == root_b:
+            return
+
+        if self.rank[root_a] < self.rank[root_b]:
+            self.parent[root_a] = root_b
+        elif self.rank[root_a] > self.rank[root_b]:
+            self.parent[root_b] = root_a
+        else:
+            self.parent[root_b] = root_a
+            self.rank[root_a] += 1
+
+
+def _parse_points(data: List[str]) -> List[Tuple[int, int, int]]:
+    points: List[Tuple[int, int, int]] = []
+    for line in data:
+        if line.count(",") != 2:
+            continue
+        x_str, y_str, z_str = line.split(",")
+        points.append((int(x_str), int(y_str), int(z_str)))
+    return points
+
+
+def _distance_squared(a: Tuple[int, int, int], b: Tuple[int, int, int]) -> int:
+    return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2
 
 
 def part1(data: List[str]) -> int:
-    return 0
+    points = _parse_points(data)
+    n = len(points)
+    if n < 3:
+        return 0
+
+    def pairs():
+        for i in range(n):
+            for j in range(i + 1, n):
+                yield (_distance_squared(points[i], points[j]), i, j)
+
+    connection_target = 10 if n <= 20 else 1000
+    shortest_pairs = heapq.nsmallest(min(connection_target, n * (n - 1) // 2), pairs())
+
+    uf = UnionFind(n)
+    for _, i, j in shortest_pairs:
+        uf.union(i, j)
+
+    component_sizes = {}
+    for node in range(n):
+        root = uf.find(node)
+        component_sizes[root] = component_sizes.get(root, 0) + 1
+
+    largest = sorted(component_sizes.values(), reverse=True)
+    if len(largest) < 3:
+        return 0
+
+    return largest[0] * largest[1] * largest[2]
 
 
 def part2(data: List[str]) -> int:
--- tests/test_day08.py
+++ tests/test_day08.py
@@ -5,14 +5,14 @@ from aoc2025.day08 import part1, part2
 
 def test_part1a(read_input):
     data = read_input("data/day08_part1.txt")
-    expected = 0
+    expected = 40
     actual = part1(data)
     assert actual == expected
 
 
 def test_part1(read_input):
     data = read_input("data/day08.txt")
-    expected = 0
+    expected = 57564
     actual = part1(data)
     assert actual == expected
 
