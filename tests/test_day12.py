c04e9a213f476163f7f733a289653ae29f246a55
Jacob Lorenzen <jacob.lorenzen@velux.com>
2026-08-04 08:04:57 +0200

    chore: setup for day12

---
+++ tests/test_day12.py
@@ -0,0 +1,31 @@
+"""Tests for Day 12"""
+
+from aoc2025.day12 import part1, part2
+
+
+def test_part1a(read_input):
+    data = read_input("data/day12_part1.txt")
+    expected = 0
+    actual = part1(data)
+    assert actual == expected
+
+
+def test_part1(read_input):
+    data = read_input("data/day12.txt")
+    expected = 0
+    actual = part1(data)
+    assert actual == expected
+
+
+def test_part2a(read_input):
+    data = read_input("data/day12_part1.txt")
+    expected = 0
+    actual = part2(data)
+    assert actual == expected
+
+
+def test_part2(read_input):
+    data = read_input("data/day12.txt")
+    expected = 0
+    actual = part2(data)
+    assert actual == expected
